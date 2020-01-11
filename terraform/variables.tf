709488e91fbae8603016189cd2bcb1509f35a0e9
rmntrvn <urometh3@gmail.com>
2020-01-11 16:15:49 +0400

    Complete HW#7 terraform-1. Created files for up infrastructure

---
+++ terraform/variables.tf
@@ -0,0 +1,38 @@
+variable project {
+  description = "Project ID"
+}
+variable region {
+  description = "Region"
+  # Значение по умолчанию
+  default = "europe-west1"
+}
+
+variable zone_instance {
+  description = "Zone for instance. Default is europe-west1-b"
+  default     = "europe-west1-b"
+}
+
+variable rmntrvn_private_key_path {
+  #
+  description = "Path to the private key used for ssh access"
+}
+
+variable rmntrvn_public_key_path {
+  # Описание переменной
+  description = "Path to the public key used for ssh access"
+}
+
+variable disk_image {
+  description = "Disk image"
+}
+
+variable app_name {
+  description = "Name of each application"
+  default     = "reddit-base-app"
+}
+
+variable count_instances {
+  type        = number
+  description = "Count of instances"
+  default     = 2
+}
